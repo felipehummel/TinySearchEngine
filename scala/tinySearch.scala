6f8d740b0dca78ecf04df5dbbc021623be28bbed
Felipe Hummel <felipehummel@gmail.com>
2011-06-22 19:14:22 -0400

    Inserted freakinTinySearch.scala with an smaller code

--- scala/tinySearch.scala
+++ scala/tinySearch.scala
@@ -10,15 +10,17 @@ class Index(val tokenizer: Tokenizer) {
     def getDocCount(term:String) = invertedIndex.getOrElse(term, Nil).size
     def index(doc:String) {
         dataset += doc
-        for(term <- tokenizer.tokenize(doc); val list = invertedIndex.getOrElse(term, Nil)) 
-            if (list != Nil && list.head.docId == dataset.size-1) 
+        for(term <- tokenizer.tokenize(doc)) {
+            val list = invertedIndex.getOrElse(term, Nil)
+            if (list != Nil && list.head.docId == dataset.size - 1) 
                 list.head.tf += 1
             else 
-                invertedIndex.put(term, Posting(dataset.size-1, 1) :: list)
+                invertedIndex.put(term, Posting(dataset.size - 1, 1) :: list)
+        }
     }
 }
 class Searcher(val index:Index, val tokenizer:Tokenizer) {
-    def docNorma(docId:Int) : Float =  
+    def docNorma(docId:Int) : Float = 
         scala.math.sqrt(tokenizer.tokenize(index.dataset(docId)).foldLeft(0F)( (accum, t) => accum + (idf(t)*idf(t)))).asInstanceOf[Float]
         
     def idf(term:String) = (scala.math.log(index.dataset.size / index.getDocCount(term)).asInstanceOf[Float])
@@ -26,8 +28,11 @@ class Searcher(val index:Index, val tokenizer:Tokenizer) {
         val tokenizedQuery = tokenizer.tokenize(q)
         var accums = new collection.mutable.HashMap[Int, Float]
         for(i <- 0 until tokenizedQuery.size; val list = index.invertedIndex.getOrElse(tokenizedQuery(i), Nil)) 
-            for(posting <- list) 
+            for(posting <- list) {
                 accums.put(posting.docId, accums.getOrElse(posting.docId, 0F) + posting.tf * idf(tokenizedQuery(i))*idf(tokenizedQuery(i)))
+                if (posting.docId == 14967)
+                    println(posting+": "+index.dataset(14967))
+            }
         val results = accums.map(d => SearchResult(d._1, index.dataset(d._1), d._2 / docNorma(d._1))).toList
         results.sortWith( _.score > _.score).take(numResults)
     }  
