a8f4893a2362b495e50cc26abdcd285be15dce46
Felipe Hummel <felipehummel@gmail.com>
2015-04-27 01:00:15 -0300

    Style changes in Scala version

--- scala/tinySearch.scala
+++ scala/tinySearch.scala
@@ -11,9 +11,8 @@ case class SimpleTokenizer(regex: String = "[^a-z0-9äöüáéíóúãâêîôû
 }
 
 class Index(val tokenizer: Tokenizer,
-            val invertedIndex: InvertedIndex = Map.empty,
-            val dataset: IndexedSeq[String] = Vector.empty) {
-  def docCount(term: String) = invertedIndex.getOrElse(term, Nil).size
+            private val invertedIndex: InvertedIndex = Map.empty,
+            private val dataset: IndexedSeq[String] = Vector.empty) {
   def index(doc: String): Index = {
     val wordCounts = tokenizer(doc).groupBy(identity).mapValues(_.size)
     var newInverted = invertedIndex
@@ -23,21 +22,27 @@ class Index(val tokenizer: Tokenizer,
     }
     new Index(tokenizer, newInverted, dataset :+ doc)
   }
+
+  def size = invertedIndex.size
+  def doc(id: Int) = dataset(id)
+  def postings(term: String): List[Posting] =
+    invertedIndex.getOrElse(term, Nil)
+  def docCount(term: String) = postings(term).size
 }
 
 class Searcher(index: Index) {
   def docNorm(docId: Int) = {
-    val docTerms = tokenizer(index.dataset(docId))
+    val docTerms = index.tokenizer(index.doc(docId))
     math.sqrt( docTerms.map( term => math.pow(idf(term), 2) ).sum )
   }
 
   def idf(term: String) =
-    math.log(index.dataset.size.toDouble / index.docCount(term).toDouble)
+    math.log(index.size.toDouble / index.docCount(term).toDouble)
 
   def searchOR(q: String, topK: Int = 10) = {
     val accums = new collection.mutable.HashMap[Int, Double].withDefaultValue(0D) //Map[docId -> Score]
     for (term <- index.tokenizer(q)) {
-        for (posting <- index.invertedIndex.getOrElse(term, Nil)) {
+        for (posting <- index.postings(term)) {
             accums.put(posting.docId, accums(posting.docId) + posting.tf * math.pow(idf(term),2))
         }
     }
@@ -46,7 +51,7 @@ class Searcher(index: Index) {
 
   private def accumToResult(docIdAndScore: (Int, Double)): Result = {
     val (docId, score) = docIdAndScore
-    Result(docId, index.dataset(docId), score / docNorm(docId))
+    Result(docId, index.doc(docId), score / docNorm(docId))
   }
 }
 
